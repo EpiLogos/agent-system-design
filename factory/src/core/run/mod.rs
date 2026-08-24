cf0d3b6ec49d332fcc8d241e2fb09661d65f10bc
Epii <frank.g.taylor97@gmail.com>
2026-08-24 22:29:47 +0200

    feat(factory): expose Run Thought field types (#163)

--- factory/src/core/run/mod.rs
+++ factory/src/core/run/mod.rs
@@ -1,14 +1,19 @@
 mod model;
 mod projection;
 mod refs;
+mod thought;
 mod topology;
 
 pub use model::{
     CommandOutcome, Project, Run, RunContractError, RunLifecycle, RunMutationAuthority,
-    RunRegistry, RunTopologyCommand, WriteAuthority,
+    RunRegistry, RunThoughtCommand, RunThoughtOutcome, RunTopologyCommand, WriteAuthority,
 };
 pub use projection::{resolve_run_projection, RunProjectionIdentity};
 pub use refs::{ProjectRef, RunMapAddress, RunRef, TypedRefError};
+pub use thought::{
+    PassageAnchor, RunThought, RunThoughtField, RunThoughtId, RunThoughtLifecycle, ThoughtFieldError,
+    ThoughtProducer,
+};
 pub use topology::{
     EdgeKind, NodeId, NodeKind, NodeState, RunMap, TopologyEdge, TopologyError, TopologyMutation,
     TopologyNode,
